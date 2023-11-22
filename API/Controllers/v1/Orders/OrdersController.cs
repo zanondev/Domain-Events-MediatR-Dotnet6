0727abe2f05c2ea0bd44535af3bf80010bcb925a
Lucas <szanonn@gmail.com>
2023-11-21 22:51:56 -0300

    feat: adicionando controller

---
+++ API/Controllers/v1/Orders/OrdersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.v1.Orders
+{
+    public class OrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
